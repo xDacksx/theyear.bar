f5c05a9125f8bc8b19aa0f6496e87c6eb25381e2
Diego Zamora <diegozar02@gmail.com>
2024-01-08 02:24:29 -0600

    create and implement basic styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import { Router } from "./Router";
+import React from "react";
+
+import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
